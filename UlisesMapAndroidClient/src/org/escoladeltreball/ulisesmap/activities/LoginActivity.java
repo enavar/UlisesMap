4f018ee996700bf813ad5414f159167f8081badb
Alejandro Sánchez <alexfer.91@gmail.com>
2014-04-24 18:26:24 +0200

    Add activity_login and activity_menu. Modificated AndroidManifest.

---
+++ UlisesMapAndroidClient/src/org/escoladeltreball/ulisesmap/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package org.escoladeltreball.ulisesmap.activities;
+
+
+import org.escoladeltreball.ulisesmap.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+	}
+}
