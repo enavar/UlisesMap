e0f9d977ed361fbd763f0bfe2fd49850f76479d0
Oleksandr Dovbysh <alexdovbysh@gmail.com>
2014-04-24 20:03:05 +0200

    adding activity and xml for show points of Interest

---
+++ UlisesMapAndroidClient/src/org/escoladeltreball/ulisesmap/ShowPointsActivity.java
@@ -0,0 +1,52 @@
+package org.escoladeltreball.ulisesmap;
+
+import java.util.ArrayList;
+
+import org.osmdroid.util.GeoPoint;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ShowPointsActivity extends Activity {
+	
+	ArrayList<Point> points;
+	private LayoutInflater layoutInflater;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		//use for testing
+		getPoints();		
+		//android:name="org.escoladeltreball.ulisesmap.activities.LoginActivity"
+		//		android:label="@string/title_activity_login" >
+		
+		
+		
+		
+	}
+	
+	private ArrayList<Point> getPoints() {
+		points = new ArrayList<Point>();
+		
+		GeoPoint gp1 = new GeoPoint(41.403, 2.174);
+		GeoPoint gp2 = new GeoPoint(41.383, 2.176);
+		GeoPoint gp3 = new GeoPoint(41.391, 2.180);
+
+		Point sf = new Point("Sagrada Familia", gp1);
+		Point cathedral = new Point("Cathedral", gp2);
+		Point arc = new Point("Arc de Trium", gp3);
+		
+		points.add(sf);
+		points.add(cathedral);
+		points.add(arc);
+		
+		return points;
+		
+	}
+
+}
---
+++ UlisesMapAndroidClient/src/org/escoladeltreball/ulisesmap/adapters/showPointsAdapter.java
@@ -0,0 +1,48 @@
+package org.escoladeltreball.ulisesmap.adapters;
+
+import java.util.ArrayList;
+
+import org.escoladeltreball.ulisesmap.Point;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class showPointsAdapter extends BaseAdapter {
+
+	ArrayList<Point> points;
+	private LayoutInflater layoutInflater;
+
+	public static class ViewHolder {
+		protected ImageView image;
+		protected TextView name, coord, description;
+
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
