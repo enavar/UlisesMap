9d4f0de1057ac4d50c31c47527ab3da708cf5177
Oleksandr Dovbysh <alexdovbysh@gmail.com>
2014-04-25 17:27:03 +0200

    add listview and connection with ShowPointsAdapter

--- UlisesMapAndroidClient/src/org/escoladeltreball/ulisesmap/MapActivity.java
+++ UlisesMapAndroidClient/src/org/escoladeltreball/ulisesmap/MapActivity.java
@@ -32,7 +32,7 @@ public class MapActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_showpoints);
 		
 		//get instance of a map
 		map = (MapView) findViewById(R.id.mapView);	
--- UlisesMapAndroidClient/src/org/escoladeltreball/ulisesmap/ShowPointsActivity.java
+++ UlisesMapAndroidClient/src/org/escoladeltreball/ulisesmap/ShowPointsActivity.java
@@ -2,23 +2,25 @@ package org.escoladeltreball.ulisesmap;
 
 import java.util.ArrayList;
 
+import org.escoladeltreball.ulisesmap.adapters.ShowPointsAdapter;
 import org.osmdroid.util.GeoPoint;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class ShowPointsActivity extends Activity {
 	
 	ArrayList<Point> points;
-	private LayoutInflater layoutInflater;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_showpoints);
 		
 		//use for testing
 		getPoints();		
@@ -26,26 +28,29 @@ public class ShowPointsActivity extends Activity {
 		//		android:label="@string/title_activity_login" >
 		
 		
-		
+		ListView list = (ListView) findViewById(R.id.listView1);
+        LayoutInflater layoutInflater = (LayoutInflater)this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        ShowPointsAdapter adapter = new ShowPointsAdapter(points, layoutInflater);
+        list.setAdapter(adapter);
+
+        list.setTextFilterEnabled(true);
 		
 	}
 	
-	private ArrayList<Point> getPoints() {
+	private void getPoints() {
 		points = new ArrayList<Point>();
 		
 		GeoPoint gp1 = new GeoPoint(41.403, 2.174);
 		GeoPoint gp2 = new GeoPoint(41.383, 2.176);
 		GeoPoint gp3 = new GeoPoint(41.391, 2.180);
-
-		Point sf = new Point("Sagrada Familia", gp1);
-		Point cathedral = new Point("Cathedral", gp2);
-		Point arc = new Point("Arc de Trium", gp3);
+		String image = "http://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Sagrada_Familia_01.jpg/330px-Sagrada_Familia_01.jpg";
+		Point sf = new Point("Sagrada Familia", gp1, image, "desc");
+		Point cathedral = new Point("Cathedral", gp2, image, "desc");
+		Point arc = new Point("Arc de Trium", gp3, image, "desc");
 		
 		points.add(sf);
 		points.add(cathedral);
 		points.add(arc);
-		
-		return points;
 		
 	}
 
