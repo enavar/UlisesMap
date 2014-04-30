411ab77e0d9109dcd4a55a65ac952b9422fbcc3c
Sheila Pérez <sheila.prz.d@gmail.com>
2014-04-30 20:45:51 +0200

    added ClientRoutesDescription

--- UlisesMapAndroidClient/src/org/escoladeltreball/ulisesmap/connections/ClientRoutesDescription.java
+++ UlisesMapAndroidClient/src/org/escoladeltreball/ulisesmap/connections/ClientRoutesDescription.java
@@ -3,44 +3,40 @@ package org.escoladeltreball.ulisesmap.connections;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.URL;
 import java.net.URLConnection;
 
 import org.apache.http.client.ClientProtocolException;
-import org.json.JSONObject;
 
 import android.os.AsyncTask;
 
-public class ClientPointsRoutes extends AsyncTask<JSONObject, Integer, String> {
+public class ClientRoutesDescription extends AsyncTask<String, Integer, String> {
 	
-	public static final String SERVLET_ROUTES = "ServletRoutes";
-	public static final String SERVLET_POINTS = "ServletPoints";
-	private static String URL = "http://wiam2-ulisesmap.rhcloud.com/";
-	private String myURL;
-	private String array;
+	private static String URL = "http://wiam2-ulisesmap.rhcloud.com/ServletRoutesDescription";
+	private String dictionary;
 	
-	public ClientPointsRoutes(String nameServlet) {
-		super();
-		this.myURL = URL + nameServlet;
-	}
-
-	protected String doInBackground(JSONObject... jsonObjects) {
+	@Override
+	protected String doInBackground(String... idRoute) {
 		try {
-			URL url = new URL(myURL);
+			URL url = new URL(URL);
 			URLConnection connection = url.openConnection();
 			connection.setDoOutput(true);
+			OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
+			out.write(idRoute[0].toString());
+			out.close();
 			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			array=in.readLine();
+			dictionary=in.readLine();
 		} catch (ClientProtocolException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return array;
+		return dictionary;
 	}
-
+	
 	public String getResponse() {
-		return array;
+		return dictionary;
 	}
 
 }
