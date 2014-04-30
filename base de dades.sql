caba0e3fed120047a3ddde85a5f4e9edec789f8c
enavar <eggy_enj@hotmail.com>
2014-04-30 21:11:41 +0200

    new database distribution

--- base de dades.sql
+++ base de dades.sql
@@ -1,8 +1,7 @@
-create table users (pk int serial,name varchar(20),password varchar(10),PRIMARY KEY(pk));
-alter table users ADD CONSTRAINT c_unique UNIQUE (name);
+create table users (name varchar(20),password varchar(10),PRIMARY KEY(name));
 create table routes (name varchar(15),description text,PRIMARY KEY(name));
-create table points (pk serial,name varchar(15),lat numeric(5,3),lon numeric(5,3),description text,image varchar(20),PRIMARY KEY(pk));
-create table valoration (def int,fk_route varchar(15) references routes(name),fk_user varchar(20) references users(name));
-create table comments (def text,fk_route varchar(15) references routes(name),fk_user varchar(20) references users(name));
+create table points (name varchar(15),lat numeric(5,3),lon numeric(5,3),description text,image varchar(20),PRIMARY KEY(name));
+create table valoration (def int,fk_route varchar(15) references routes(name),fk_user varchar(20) references users(name),PRIMARY KEY (fk_user,fk_route));
+create table comments (def text,fk_route varchar(15) references routes(name),fk_user varchar(20) references users(name),PRIMARY KEY (fk_user,fk_route));
 
 
