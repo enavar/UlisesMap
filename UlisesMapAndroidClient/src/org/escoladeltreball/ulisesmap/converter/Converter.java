e983a6a65b70c0464b77522e6694c36027a1d177
Sheila Pérez <sheila.prz.d@gmail.com>
2014-05-07 18:32:18 +0200

    Corrected error of Converter when point hasn't url.

--- UlisesMapAndroidClient/src/org/escoladeltreball/ulisesmap/converter/Converter.java
+++ UlisesMapAndroidClient/src/org/escoladeltreball/ulisesmap/converter/Converter.java
@@ -50,19 +50,18 @@ public class Converter {
 			JSONArray jSONArrayPoints = new JSONArray(arrayPoints);
 			int size = jSONArrayPoints.length();
 			for (int i = 0; i < size; i++) {
+				System.out.println(i + "/" + size);
 				JSONObject objPoint = jSONArrayPoints.getJSONObject(i);
 				String name = objPoint.getString(Point.FIELD_NAME);
 				double lat = objPoint.getDouble(Point.FIELD_LAT);
 				double lon = objPoint.getDouble(Point.FIELD_LON);
 				String street = objPoint.getString(Point.FIELD_STREET);
 				String description = objPoint.getString(Point.FIELD_DESCRIPTION);
-				String url = objPoint.getString(Point.FIELD_URL);
+				String url = (!objPoint.isNull(Point.FIELD_URL)) ? objPoint.getString(Point.FIELD_URL) : "";
 				String image = objPoint.getString(Point.FIELD_IMAGE);
 				GeoPoint geoPoint = new GeoPoint(lat, lon);
 				Point point = new Point(name, geoPoint, street, description, image, url);
-				boolean add = points.add(point);
-				System.out.println("" + add + " " + name);
-				System.out.println(objPoint);
+				points.add(point);
 			}
 		} catch (JSONException e) {
 			e.printStackTrace();
