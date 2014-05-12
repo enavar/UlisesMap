021c61cddf4d3c8d1fe0c6e0d3084445531a7bad
enavar <en.joseph.adm@gmail.com>
2014-05-12 16:57:17 +0200

    add a check for existing previous shared preferences in login add a sending comments and valorations in showComments

--- UlisesMapAndroidClient/src/org/escoladeltreball/ulisesmap/activities/ShowCommentsActivity.java
+++ UlisesMapAndroidClient/src/org/escoladeltreball/ulisesmap/activities/ShowCommentsActivity.java
@@ -1,11 +1,14 @@
 package org.escoladeltreball.ulisesmap.activities;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 import org.escoladeltreball.ulisesmap.BaseActivity;
 import org.escoladeltreball.ulisesmap.R;
 import org.escoladeltreball.ulisesmap.adapters.ShowCommentsAdapter;
+import org.escoladeltreball.ulisesmap.connections.Client;
 import org.escoladeltreball.ulisesmap.model.CommentValoration;
+import org.escoladeltreball.ulisesmap.model.Settings;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -33,21 +36,29 @@ public class ShowCommentsActivity extends BaseActivity {
         list.setTextFilterEnabled(true);
 	}
 	
-	public void addCommentValoration(View view) throws JSONException {
-		// capture user from sharedprefs
-		String userName = "";
-		String routeName = "";
+	public void addCommentValoration(View view) throws JSONException, InterruptedException, ExecutionException {
+		// capture user and route from Settings
+		String userName = Settings.userName;
+		insertComment.put("fk_user", userName);
+		String routeName = Settings.routeName;
+		insertComment.put("fk_route", routeName);
+		// prepare client output
+		Client client = null;
 		if (view.equals(R.id.addComment)) {
 			TextView tv = (TextView) findViewById(R.id.textView1);
 			String commentText = tv.getText().toString();
 			insertComment.put("def", commentText);
+			client = new Client(Client.SERVLET_COMMENT_INSERT, true);
+			
 		} else {
 			RatingBar rb = (RatingBar) findViewById(R.id.ratingBar1);
 			int valoration = rb.getNumStars();
 			insertComment.put("def", valoration);
+			client = new Client(Client.SERVLET_VALORATION_INSERT, true);
 		}
-		insertComment.put("fk_user", userName);
-		insertComment.put("fk_route", routeName);
+		// output to server
+		String out = insertComment.toString();
+		client.execute(out).get();
 		
 	}
 
